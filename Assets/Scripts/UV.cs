609ade772e0182b061c2378a5f6fb1c1af9c8e00
K01u5 <natwhite@uat.edu>
2019-04-26 05:31:11 -0700

    Initial commit

---
+++ Assets/Scripts/UV.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class UV
+{
+
+    private static string[] sendDataToJar(string func, string[] dataToSend)
+    {
+        // Create object array that will be passed to the CallStatic method
+        object[] objParams = new object[1];
+        string[] innerData = new string[dataToSend.Length];
+
+        // By assigning item 1 in the object[], you are preventing the compiler from parsing them into the function call, so the Unity code sees the string[] and creates the Java signature properly
+        objParams[0] = innerData;
+
+        // Just for grins, let's also receive a string[] back from java (String[] in java, string[] in C#) and do a manual conversion on it treating it like an Object
+        AndroidJavaClass classInst = new AndroidJavaClass("com.duli_industries.uv_utilities.UV");
+        AndroidJavaObject returnedObjectArray = classInst.CallStatic<AndroidJavaObject>(func, objParams);
+        string[] resultsArray = null;
+
+        if (returnedObjectArray != null && returnedObjectArray.GetRawObject().ToInt32() != 0)
+        {
+            resultsArray = AndroidJNIHelper.ConvertFromJNIArray<string[]>(returnedObjectArray.GetRawObject());
+            // Debug.Log(string.Format("Got returned {0}", returnedStrings));
+        }
+
+        return resultsArray;
+    }
+
+    public static string testPrintStatic()
+    {
+        return sendDataToJar("testPrintStatic", new string[] { "null" })[0];
+    }
+
+    public static void open()
+    {
+        sendDataToJar("open", new string[] { "null" });
+    }
+
+    public static void close()
+    {
+        sendDataToJar("close", new string[] { "null" });
+    }
+
+    public static int get_ADC_Val()
+    {
+        return int.Parse(sendDataToJar("get_ADC_Val", new string[] { "null" })[0]);
+    }
+}
---
+++ Assets/Scripts/UVTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVTests : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Android function is to be called 2");
+
+		// UV.open();
+        string result = UV.testPrintStatic();
+		// int val = UV.get_ADC_Val();
+		// UV.close();
+
+        Debug.Log(string.Format("Result was {0}", result));
+
+        Debug.Log("Androidtion is called");
+    }
+}
